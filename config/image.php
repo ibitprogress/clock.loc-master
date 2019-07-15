528060a62a70d855f75be33c910ac1d57b6fb927
ibitprogress <52105615+ibitprogress@users.noreply.github.com>
2019-07-16 00:19:38 +0300

    Add files via upload

---
+++ config/image.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | ClockImage Driver
+    |--------------------------------------------------------------------------
+    |
+    | Intervention ClockImage supports "GD Library" and "Imagick" to process images
+    | internally. You may choose one of them according to your PHP
+    | configuration. By default PHP's "GD Library" implementation is used.
+    |
+    | Supported: "gd", "imagick"
+    |
+    */
+
+    'driver' => 'gd'
+
+];
---
+++ config/novaposhta.php
@@ -0,0 +1,8 @@
+<?php
+Use NovaPoshta\Config;
+
+return [
+    Config::setApiKey('b606f49bb19095dbbb36e506eb8b58fa'),
+    Config::setFormat(Config::FORMAT_JSONRPC2),
+    Config::setLanguage(Config::LANGUAGE_UA),
+];
